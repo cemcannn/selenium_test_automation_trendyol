ed8e143d32f59df7578de8ad4604f0fc25c836e8
cemcannn <cmcan@windowslive.com>
2022-02-15 00:38:31 +0300

    Main1 | Directory names, Package names, class names, function names is fixed as underscore which were seperated with Camel Case changes

--- TestData/LoginData.py
+++
@@ -1,2 +0,0 @@
-class LoginData:
-    test_e2e_data = [{"userName": "denemehesaplari58@gmail.com", "password": "123456789Cc", "searchBar": "bornoz", "brandFilter": "eng"}]
--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -3,33 +3,33 @@ import time
 import pytest
 from selenium.webdriver.common.by import By
 
-from PageObject.HomePage import HomePage
-from PageObject.LoginPage import LoginPage
-from PageObject.ProductPage import ProductPage
-from TestData.LoginData import LoginData
-from Utilities.BaseClass import BaseClass
+from page_object.Home_page import Home_page
+from page_object.Login_page import Login_page
+from page_object.Product_page import Product_page
+from test_data.Login_data import Login_data
+from utilities.Base_class import Base_class
 
 
-class TestOne(BaseClass):
-    def test_e2e(self, getData):
-        log = self.getLogger()
+class TestOne(Base_class):
+    def test_e2e(self, get_data):
+        log = self.get_logger()
         self.driver.implicitly_wait(5)
         self.driver.find_element(By.CSS_SELECTOR, "path[id='Combined-Shape']").click()
-        homePage = HomePage(self.driver)
-        self.actionMoveToElement(homePage.getLoginMenu()).perform()
-        self.actionMoveToElement(homePage.getLoginSubMenu()).click().perform()
-        loginPage = LoginPage(self.driver)
-        loginPage.getUserName().send_keys(getData["userName"])
-        loginPage.getPassword().send_keys(getData["password"])
+        home_page = Home_page(self.driver)
+        self.action_move_to_element(home_page.get_login_menu()).perform()
+        self.action_move_to_element(home_page.get_login_sub_menu()).click().perform()
+        login_page = Login_page(self.driver)
+        login_page.get_user_name().send_keys(get_data["userName"])
+        login_page.get_password().send_keys(get_data["password"])
         log.info("Giriş yapılıyor.")
-        mainPage = loginPage.getSubmit()
+        main_page = login_page.get_submit()
         time.sleep(2)
-        mainPage.getSearchBar().send_keys(getData["searchBar"])
-        mainPage.getSuggest()
+        main_page.get_search_bar().send_keys(get_data["searchBar"])
+        main_page.get_suggest()
         # mouse click on blank area for pass pop-up
-        mainPage.getFreeClick()
-        mainPage.getBrandFilter().send_keys(getData["brandFilter"])
-        brands = mainPage.getBrandTitles()
+        main_page.get_free_click()
+        main_page.get_brand_filter().send_keys(get_data["brandFilter"])
+        brands = main_page.get_brand_titles()
         for brand in brands:
             if brand.text == "English Home":
                 # //div[@class='fltr-item-text']/parent::a/div[@class='chckbox']
@@ -38,7 +38,7 @@ class TestOne(BaseClass):
 
         time.sleep(2)
         rating_list = []
-        products = mainPage.getRatingCounts()
+        products = main_page.get_rating_counts()
         for product in products:
             rating_list.append(product.text)
             if product.text == max(rating_list):
@@ -46,15 +46,15 @@ class TestOne(BaseClass):
                 product.click()
 
         self.driver.switch_to.window(self.driver.window_handles[1])
-        productPage = ProductPage(self.driver)
+        product_page = Product_page(self.driver)
         log.info("Ürün sepete ekleniyor.")
-        productPage.getAddBasket()
+        product_page.get_add_basket()
         self.driver.switch_to.window(self.driver.window_handles[0])
-        checkOutPage = mainPage.getLinkBasket()
-        log.info("Ürün açıklaması " + checkOutPage.getProductDescription())
-        assert "Dinosaurs Kadife Çocuk Bornoz 8-10 Yaş Yeşil" in checkOutPage.getProductDescription()
+        check_out_page = main_page.get_link_basket()
+        log.info("Ürün açıklaması " + check_out_page.get_product_description())
+        assert "Dinosaurs Kadife Çocuk Bornoz 8-10 Yaş Yeşil" in check_out_page.get_product_description()
 
-    @pytest.fixture(params=LoginData.test_e2e_data)
+    @pytest.fixture(params=Login_data.test_e2e_data)
     def getData(self, request):
         return request.param
 
